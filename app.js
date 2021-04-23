651456a48699007480e9f491e314d00c805a127a
Vovan1608 <57871837+Vovan1608@users.noreply.github.com>
2021-04-23 14:09:27 +0300

    done

--- app.js
+++ app.js
@@ -17,3 +17,15 @@ const form = document.querySelector(".lorem-form");
 const amount = document.getElementById("amount");
 const result = document.querySelector(".lorem-text");
 
+form.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const value = Number(amount.value);
+	const random = Math.floor(Math.random() * text.length);
+
+	if (isNaN(value) || value < 0 || value > 9) {
+		result.innerHTML = `<p class="result">${text[random]}</p>`;
+	} else {
+		let tempText = text.slice(0, value);
+		result.innerHTML = tempText.map(item => `<p class="result">${item}</p>`).join("");
+	}
+});
